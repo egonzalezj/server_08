5f1ecf67af448929d96298ee1dc772392ede0a22
egonzalezj <eli.jvn@gmail.com>
2016-10-24 22:57:44 -0600

    Reading parameters

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*server.js*/
 
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var hostname = 'webrtctest2.zapto.org';
 var port = 80;
@@ -8,7 +9,8 @@ var port = 80;
 var app = express();
 
 app.use('/static',express.static('public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'jade')
 
 app.get('/', function(req, res) {
@@ -16,7 +18,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function (req, res) {
-  res.render('login')
+  res.render('login');
+})
+
+app.post('/users', function (req, res) {
+  console.log('Password: ' + req.body.password);
+  console.log('Email: ' + req.body.email);
+  res.send('Data received');
 })
 
 app.listen(port, hostname, function () {
